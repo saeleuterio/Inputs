eef0756461eab41ff3d692cd180632d40ca08016
saeleuterio <saeleuterio@hotmail.com>
2024-08-03 15:33:48 -0300

    JS

--- signodehoje.js
+++ signodehoje.js
@@ -24,15 +24,27 @@ const retorna_signo = (signos, data) => {
         let data_fim_signo =
          new Date(ano + "-" + signo["DataFim"] + " 23:59:59");
 
-        if ( data >= data_inicio_signo && data <= data_fim_signo ){
-            return signo["Nome"];
+        let tipo_comparacao = signo["DataInicio"] == "12-22" ? "or" : "and";
+
+        if ( tipo_comparacao == "and"){
+
+            if ( data >= data_inicio_signo && data <= data_fim_signo ){
+                return signo["Nome"];
+            }
+
+        } else if (tipo_comparacao == "or"){
+
+            if ( data >= data_inicio_signo || data <= data_fim_signo ){
+                return signo["Nome"];
+            }
+
         }
 
     }
 
 }
 
-let data_app = new Date("2020-07-28 00:00:00");
+let data_app = new Date();
 
 const nome_signo = retorna_signo(colecao_signos, data_app);
 
