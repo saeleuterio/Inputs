e5aa0e588e1d93fb00762b423c95115c03802647
saeleuterio <saeleuterio@hotmail.com>
2024-08-15 16:30:59 -0300

    JS

--- exercicio.js
+++ exercicio.js
@@ -1,13 +1,3 @@
-const hoje = new Date();
-const diaSemana = hoje.getDay();
-
-switch (diaSemana) {
-  case 0:
-  case 2:
-  case 4:
-  case 6:
-    console.log('Plantão');
-    break;
-  default:
-    console.log("Folga");
-}
+const linguagem = "JavaScript";
+linguagem = "JS";
+console.log(linguagem);
